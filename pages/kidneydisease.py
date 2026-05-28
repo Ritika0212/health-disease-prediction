999d46750eee5bb8bf5c10b75f2cf2b4ef5fd92a
RITIKA THAKUR <ritikathakurr012@gmail.com>
2026-05-29 01:30:46 +0530

    Update kidneydisease.py

--- pages/kidneydisease.py
+++ pages/kidneydisease.py
@@ -1,1 +1,10 @@
 
+import streamlit as st
+
+st.title("🧪 Kidney Disease Prediction")
+
+bp = st.number_input("Blood Pressure")
+sugar = st.number_input("Sugar Level")
+
+if st.button("Predict"):
+    st.success("Kidney Prediction Done")

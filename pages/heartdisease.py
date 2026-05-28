df5b1c1844494c486366fc0c63038f93db4aad50
RITIKA THAKUR <ritikathakurr012@gmail.com>
2026-05-29 01:30:06 +0530

    Update heartdisease.py

--- pages/heartdisease.py
+++ pages/heartdisease.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title("❤️ Heart Disease Prediction")
+
+age = st.number_input("Age")
+chol = st.number_input("Cholesterol")
+
+if st.button("Predict"):
+    st.success("Heart Prediction Done")
